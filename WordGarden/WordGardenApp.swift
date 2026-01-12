78521917688de4e6299857b7e9b0b2e789a91ad9
Azamorano8228 <zamoranoandres8228@gmail.com>
2026-01-12 10:42:25 -0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by ZAMORANO, ANDRES on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
